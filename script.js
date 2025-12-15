22d6c0e5f827e3e13ab79358436778beaea544cb
alexandrexan <xampsbeatz@gmail.com>
2025-12-15 20:38:35 -0300

    Botão back to top

--- script.js
+++ script.js
@@ -63,4 +63,21 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // ? BOTÃO VOLTAR AO TOPO
+    const btn = document.getElementById('backToTop');
+
+    window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        btn.classList.add('show');
+    } else {
+        btn.classList.remove('show');
+    }
+    });
+
+    btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    });
 });
